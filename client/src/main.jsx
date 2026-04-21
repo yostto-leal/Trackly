fad1df534971b020e6f6396e7884de88cd10afd7
Ary-Leal <ariosto.leal.ncr@gmail.com>
2026-04-21 19:00:37 -0300

    fix: remove import inexistente index.css

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,10 +2,9 @@ import 'leaflet/dist/leaflet.css';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 
-// Importa o CSS do Leaflet
-import 'leaflet/dist/leaflet.css';
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
